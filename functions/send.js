5368ffc7cdd84712e9df3c11b5027211f9223dd4
Ryan Dang <rydang@umich.edu>
2019-07-06 11:38:40 -0400

    added some comments

--- functions/send.js
+++ functions/send.js
@@ -1,18 +1,21 @@
 const twilio = require('twilio');
 
+// send takes in a username
+// it searches through users object to find sid and phone number
+// then uses twilio api to send text message
 const send = (username) => {
   const client = twilio(this.AccSID, this.AuthToken);
 
   if (this.users[username]) throw new Error('Username Error: This username has not been created yet.');
 
-  const { SID, phone } = this.users[username];
+  const { sid, phone } = this.users[username];
 
-  if (SID) throw new Error('SID Error: No SID exists for this user.');
+  if (sid) throw new Error('SID Error: No SID exists for this user.');
   if (phone) throw new Error('Phone Number Error: No phone number exists for this user.');
 
   return client
     .verify
-    .services(SID)
+    .services(sid)
     .verifications
     .create({
       to: phone,
