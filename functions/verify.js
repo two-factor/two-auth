55b42b85532906bf7ce5ebc2cb3c373dd4aa8f23
Ryan Dang <rydang@umich.edu>
2019-07-06 11:56:15 -0400

    started verify

--- functions/verify.js
+++ functions/verify.js
@@ -1,7 +1,21 @@
 const twilio = require('twilio')
 
-const verify = (username,code) => {
+const verify = (username, code) => {
+  const client = twilio(this.AccSID, this.AuthToken);
 
+  if (!this.users[username]) throw new Error('Username Error: This username has not been created yet.');
+
+  const { sid, phone } = this.users[username];
+
+  return client
+    .verify
+    .services(sid)
+    .verificationChecks
+    .create({
+      to: phone,
+      code,
+    })
+    .then(verificationCheck => verificationCheck.status);
 }
 
 module.exports = verify;
