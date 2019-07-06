319fc4d5cee6bb473989dda28391cc497ffa99eb
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-06 18:12:38 -0400

    added mongo file structure and schema for users

---
+++ __tests__/mongoose.js
@@ -0,0 +1,5 @@
+const client = require("../index")(
+  "AC307f83011f4e85c3b80560128a3ada18",
+  "5e6536e39b68aab14033e0b2cc8b8274",
+  "mongodb://localhost/mongodb-orm"
+);
--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const twilio = require("twilio");
+const mongoose = require("mongoose");
 
 const create = require("./functions/create.js");
 const send = require("./functions/send.js");
 const verify = require("./functions/verify.js");
 
-const connect = (AccSID, AuthToken) => {
-  return new Client(AccSID, AuthToken);
+const mongooseCreate = require("./mongoose/create");
+const mongooseSend = require("./mongoose/send");
+const mongooseVerify = require("./mongoose/verify");
+
+const userSchema = require("./mongoose/userSchema");
+
+const connect = (AccSID, AuthToken, mongoURI = null) => {
+  return new Client(AccSID, AuthToken, mongoURI);
 };
 
 class Client {
-  constructor(AccSID, AuthToken) {
+  constructor(AccSID, AuthToken, mongoURI = null) {
     this.AccSID = AccSID;
     this.AuthToken = AuthToken;
     this.client = twilio(this.AccSID, this.AuthToken);
     this.users = {};
-    this.create = create;
-    this.send = send;
-    this.verify = verify;
+
+    if (mongoURI !== null) {
+      mongoose
+        .connect(mongoURI)
+        .then(db => {
+          console.log("Two Factor successfully connected to Mongo");
+        })
+        .catch(err => {
+          throw new Error("Two Factor Unable to connect to Mongo");
+        });
+      this.TwoFactorUser = mongoose.model("TwoFactorUser", userSchema);
+      this.create = mongooseCreate;
+      this.send = mongooseSend;
+      this.verify = mongooseVerify;
+    } else {
+      this.create = create;
+      this.send = send;
+      this.verify = verify;
+    }
   }
 }
 
