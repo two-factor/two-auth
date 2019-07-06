ce3f51e251dd613f4ceb7ace5ac8b0361c7e241d
Ryan Dang <rydang@umich.edu>
2019-07-06 11:42:20 -0400

    forgot about to add bang operators

--- functions/send.js
+++ functions/send.js
@@ -6,12 +6,12 @@ const twilio = require('twilio');
 const send = (username) => {
   const client = twilio(this.AccSID, this.AuthToken);
 
-  if (this.users[username]) throw new Error('Username Error: This username has not been created yet.');
+  if (!this.users[username]) throw new Error('Username Error: This username has not been created yet.');
 
   const { sid, phone } = this.users[username];
 
-  if (sid) throw new Error('SID Error: No SID exists for this user.');
-  if (phone) throw new Error('Phone Number Error: No phone number exists for this user.');
+  if (!sid) throw new Error('SID Error: No SID exists for this user.');
+  if (!phone) throw new Error('Phone Number Error: No phone number exists for this user.');
 
   return client
     .verify
