0bd996a882361359d5a378777025af7cc7cf3aa6
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-06 19:19:15 -0400

    added send and verify mongoose functions, first iteration

--- __tests__/mongoose.js
+++
@@ -1,9 +0,0 @@
-const client = require("../index")(
-  "AC307f83011f4e85c3b80560128a3ada18",
-  "5e6536e39b68aab14033e0b2cc8b8274",
-  "mongodb://localhost/twofactortest"
-);
-client
-  .create("ian", "+17604207520")
-  .then(user => console.log(user))
-  .catch(err => console.log(err));
--- mongoose/verify.js
+++ mongoose/verify.js
@@ -4,30 +4,34 @@
 // if there is a formatting error, promise rejects
 // if there is no error, resolves to be the status of the verification
 // status is true if verification succeeded, false if verification failed
-function verify(username, code) {
+
+function verify(userID, code) {
+  const TwoFactorUser = this.TwoFactorUser;
   return new Promise((resolve, reject) => {
-    if (!this.users[username])
-      reject(
-        new Error("Username Error: This username has not been created yet.")
-      );
+    TwoFactorUser.findOne({ userID })
+      .then(user => {
+        const { sid, phone } = user;
+        if (!sid) reject(new Error("SID Error: No SID exists for this user."));
+        if (!phone)
+          reject(
+            new Error(
+              "Phone Number Error: No phone number exists for this user."
+            )
+          );
 
-    const { sid, phone } = this.users[username];
-
-    if (!sid) reject(new Error("SID Error: No SID exists for this user."));
-    if (!phone)
-      reject(
-        new Error("Phone Number Error: No phone number exists for this user.")
-      );
-
-    return this.client.verify
-      .services(sid)
-      .verificationChecks.create({
-        to: phone,
-        code
+        return this.client.verify
+          .services(sid)
+          .verificationChecks.create({
+            to: phone,
+            code
+          })
+          .then(verification => {
+            if (verification.status === "approved") resolve(true);
+            return reject(false);
+          });
       })
-      .then(verification => {
-        if (verification.status === "approved") resolve(true);
-        return resolve(false);
+      .catch(err => {
+        reject(false);
       });
   });
 }
