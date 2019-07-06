59eae7726769c6d54d9e27cb8a28b08423c51176
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-06 12:06:36 -0400

    removed consolelog statements

--- functions/create.js
+++ functions/create.js
@@ -1,14 +1,10 @@
-const twilio = require("twilio");
-
 // Takes in a userID and phone number associated with that user
 // returns a promise
 // if resolved, adds a user Object with the userId, sid, and phone associated with the service for that user...
 // returns a reference to that object
 // if rejected, throws error from verify API
 function create(userID, phone) {
-  console.log("this", this);
   const client = this.client;
-  console.log(client);
   const users = this.users;
 
   return new Promise((resolve, reject) => {
