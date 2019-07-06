4ad9f68c14590147aa842b1742b71825c3066a49
Ryan Dang <rydang@umich.edu>
2019-07-06 11:27:28 -0400

    send function now sends a code through twilio to phone number

--- functions/send.js
+++ functions/send.js
@@ -1,7 +1,23 @@
-const twilio = require('twilio')
+const twilio = require('twilio');
 
 const send = (username) => {
+  const client = twilio(this.AccSID, this.AuthToken);
 
-}
+  if (this.users[username]) throw new Error('Username Error: This username has not been created yet.');
+
+  const { SID, phone } = this.users[username];
+
+  if (SID) throw new Error('SID Error: No SID exists for this user.');
+  if (phone) throw new Error('Phone Number Error: No phone number exists for this user.');
+
+  return client
+    .verify
+    .services(SID)
+    .verifications
+    .create({
+      to: phone,
+      channel: 'sms',
+    });
+};
 
 module.exports = send;
